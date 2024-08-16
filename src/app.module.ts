66354b97d6197cc6c881027be9bcb1fdaf1d8dac
majolivero <majolivero28@gmail.com>
2024-08-16 15:47:09 -0500

    Indicación de como conectar a MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost/nest-tutorial1')], 
   controllers: [AppController, TasksController],  //Aquí se agregó la clase task controller
   providers: [AppService, TasksService],
 })
 export class AppModule {}
+
+
+//En este proyecto se empieza a usa MongoDB. 
+//Link de youtube: https://www.youtube.com/watch?v=NYoCbihISxw
